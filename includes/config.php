1d004d73ebf84527a0fa167238c06332c936634b
Gilberto Leon <gilberto@ripfla.me>
2014-04-25 22:43:03 -0500

    Changing charset to db controller

--- includes/config.php
+++ includes/config.php
@@ -5,5 +5,5 @@ $db_password = "Hhft8dbT69y5pAMQ";
 $db_name = "logondb";
 
 $db = new mysqli( $db_hostname, $db_user, $db_password, $db_name );
-$db->set_charset( 'utf-8' );
+$db->set_charset( 'utf8' );
 ?>
