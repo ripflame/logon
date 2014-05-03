bd2a39267c9d0bec9aabb26ba0643c4f59ca658c
Gilberto Leon <gilberto@ripfla.me>
2014-05-03 01:19:56 -0500

    Update password reset

--- reset.php
+++ reset.php
@@ -9,12 +9,12 @@ $token_valid = false;
 $password_reset = false;
 
 if ( isset( $_POST['submit'] ) ) {
-  echo 'FUCK'; exit();
   $password = $db->real_escape_string( $_POST['password'] );
   $password_confirm = $db->real_escape_string( $_POST['password_confirm'] );
   $email = $db->real_escape_string( $_POST['email'] );
 
   if ( $password == $password_confirm ) {
+  echo 'FUCK'; exit();
     if ( update_user_password( $password, $email ) ) {
       $password_reset = true;
     }
